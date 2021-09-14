20aa2bee5a0860aa0239610b15eef10620efe8a9
edualvesz <ea-hendrix@hotmail.com>
2021-09-14 11:48:01 -0300

    Criacao de botoes customizados na view de mensalidades

--- resources/views/menu/mensalidade/index.blade.php
+++ resources/views/menu/mensalidade/index.blade.php
@@ -8,18 +8,41 @@
 
 @section('content')
     <!--<p>Welcome to this beautiful admin panel.</p>-->
-            <div class="card">
-              <div class="card-header">
-                <h3 class="card-title">Alunos ativos</h3>
-              </div>
+<div class="card">
+    <div class="card-header">
+      <h3 class="card-title">Alunos ativos</h3>
+    </div>
+    <div class="card-body">
+        <div class="row align-self-center w-100">  <!-- Itens alinhados deverão ficar nessa div -->
+            <div class="d-grid gap-2 d-md-block">
+                <!-- <a href="#" class="btn btn-success">+ Adicionar Novo</a> -->
+                <a href="#" class="btn btn-app bg-success">
+                <i class="fas fa-users"></i> + Adicionar Novo
+                </a>
+            </div>
+            <div class="col-2 mx-auto">
+                <!-- <a href="#" class="link-primary">Mensalidades Pagas</a> -->
+                <a href="#" class="btn btn-app">
+                <!-- <span class="badge bg-success">300</span> -->
+                <i class="fas fa-barcode"></i>Mensalidades Pagas 
+                </a>
+            </div>
+            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                <a href="#" class="btn btn-app bg-danger">
+                <i class="fas fa-inbox"></i> Serviços
+                </a>
+            </div>  
+        </div>
+    </div>  
               <!-- /.card-header -->
               <div class="card-body">
                 <div id="example1_wrapper" class="dataTables_wrapper dt-bootstrap4"><div class="row"><div class="col-sm-12 col-md-6">
                     
                 </div><div class="col-sm-12 col-md-6">
-                    <div id="example1_filter" class="dataTables_filter">
+                    <!-- <div id="example1_filter" class="dataTables_filter">
                         <label>Pesquisar<input type="search" class="form-control form-control-sm" placeholder="" aria-controls="example1"></label>
-                    </div>
+                    </div> -->
+                    
                 </div>
             </div>
             <div class="row">
@@ -216,7 +239,7 @@
                 </tr> -->
                 </tbody>
                  
-                </table></div></div><div class="row"><div class="col-sm-12 col-md-5"><div class="dataTables_info" id="example1_info" role="status" aria-live="polite">Mostrando 1 a 10 de 57 registros</div></div><div class="col-sm-12 col-md-7"><div class="dataTables_paginate paging_simple_numbers" id="example1_paginate"><ul class="pagination"><li class="paginate_button page-item previous disabled" id="example1_previous"><a href="#" aria-controls="example1" data-dt-idx="0" tabindex="0" class="page-link">Previous</a></li><li class="paginate_button page-item active"><a href="#" aria-controls="example1" data-dt-idx="1" tabindex="0" class="page-link">1</a></li><li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="2" tabindex="0" class="page-link">2</a></li><li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="3" tabindex="0" class="page-link">3</a></li><li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="4" tabindex="0" class="page-link">4</a></li><li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="5" tabindex="0" class="page-link">5</a></li><li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="6" tabindex="0" class="page-link">6</a></li><li class="paginate_button page-item next" id="example1_next"><a href="#" aria-controls="example1" data-dt-idx="7" tabindex="0" class="page-link">Next</a></li></ul></div></div></div></div>
+                </table></div></div><div class="row"><div class="col-sm-12 col-md-5"><div class="dataTables_info" id="example1_info" role="status" aria-live="polite">Mostrando 1 a 10 de 57 registros</div></div><div class="col-sm-12 col-md-7"><div class="dataTables_paginate paging_simple_numbers" id="example1_paginate"><ul class="pagination"><li class="paginate_button page-item previous disabled" id="example1_previous"><a href="#" aria-controls="example1" data-dt-idx="0" tabindex="0" class="page-link">Anterior</a></li><li class="paginate_button page-item active"><a href="#" aria-controls="example1" data-dt-idx="1" tabindex="0" class="page-link">1</a></li><li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="2" tabindex="0" class="page-link">2</a></li><li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="3" tabindex="0" class="page-link">3</a></li><li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="4" tabindex="0" class="page-link">4</a></li><li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="5" tabindex="0" class="page-link">5</a></li><li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="6" tabindex="0" class="page-link">6</a></li><li class="paginate_button page-item next" id="example1_next"><a href="#" aria-controls="example1" data-dt-idx="7" tabindex="0" class="page-link">Próxima</a></li></ul></div></div></div></div>
               </div>
               <!-- /.card-body -->
             </div>
