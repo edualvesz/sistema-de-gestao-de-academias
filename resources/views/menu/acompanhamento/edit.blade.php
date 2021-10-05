0597d28830e99670c325ac9905efa97c9f936cf2
edualvesz <ea-hendrix@hotmail.com>
2021-10-05 14:26:15 -0300

    Criacao do botao de acompanhamento do aluno do menu acompanhamento

--- resources/views/menu/acompanhamento/edit.blade.php
+++ resources/views/menu/acompanhamento/edit.blade.php
@@ -8,6 +8,53 @@
 
 @section('content')
     <!--<p>Welcome to this beautiful admin panel.</p>-->
+    <div class="col-12 col-sm-12">
+        <div class="card card-primary card-outline card-outline-tabs">
+            <div class="card-header p-0 border-bottom-0">
+                <ul class="nav nav-tabs" id="custom-tabs-four-tab" role="tablist">
+                    <li class="nav-item">
+                        <a class="nav-link active" id="custom-tabs-four-home-tab" data-toggle="pill" href="#custom-tabs-four-home" role="tab" aria-controls="custom-tabs-four-home" aria-selected="false">Resumo</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="custom-tabs-four-profile-tab" data-toggle="pill" href="#custom-tabs-four-profile" role="tab" aria-controls="custom-tabs-four-profile" aria-selected="false">Objetivo</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="custom-tabs-four-messages-tab" data-toggle="pill" href="#custom-tabs-four-messages" role="tab" aria-controls="custom-tabs-four-messages" aria-selected="false">Antropometria</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="custom-tabs-four-settings-tab" data-toggle="pill" href="#custom-tabs-four-settings" role="tab" aria-controls="custom-tabs-four-settings" aria-selected="true">PAR-Q e Anamnese</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="custom-tabs-four-settings-tab" data-toggle="pill" href="#custom-tabs-four-settings" role="tab" aria-controls="custom-tabs-four-settings" aria-selected="true">Ficha Treinamento</a>
+                    </li>
+                </ul>
+            </div>
+                <div class="card-body">
+                    <div class="tab-content" id="custom-tabs-four-tabContent">
+                        <div class="tab-pane fade active show" id="custom-tabs-four-home" role="tabpanel" aria-labelledby="custom-tabs-four-home-tab">
+                            <h5>Objetivo principal:</h5><br><br>
+                            <h5>Meta de peso ideal:</h5><br><br>
+                            <h5>Evolução:</h5><br><br>
+                            <h5>PAR-Q:</h5><br><br>
+                            <h5>Última avaliação física:</h5><br><br>
+                            <h5>Avaliação da composição corporal: Protocolo Pollok (Sete dobras cutâneas):</h5><br><br>
+                        </div>
+                        <div class="tab-pane fade" id="custom-tabs-four-profile" role="tabpanel" aria-labelledby="custom-tabs-four-profile-tab">
+                            
+                        </div>
+                        <div class="tab-pane fade" id="custom-tabs-four-messages" role="tabpanel" aria-labelledby="custom-tabs-four-messages-tab">
+                            
+                        </div>
+                        <div class="tab-pane fade" id="custom-tabs-four-settings" role="tabpanel" aria-labelledby="custom-tabs-four-settings-tab">
+                            
+                        </div>    
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+
+            </div>
+        </div>
 @stop
 
 @section('css')
